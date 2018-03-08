4b0d1b843d549fd13b24e0a0b083dc0143dc34a0
scolas <scolas481@gmail.com>
2018-03-07 21:08:03 -0500

    new file names

--- app/src/main/java/com/example/android/clubsconnect/MainActivity.java
+++ app/src/main/java/com/example/android/clubsconnect/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.android.clubs_connect;
+package com.example.android.clubsconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/example/android/clubsconnect/view/MessageViewHolder.java
+++ app/src/main/java/com/example/android/clubsconnect/view/MessageViewHolder.java
@@ -1,11 +1,11 @@
-package com.example.android.clubs_connect.view;
+package com.example.android.clubsconnect.view;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.android.clubs_connect.R;
-import com.example.android.clubs_connect.model.Message;
+import com.example.android.clubsconnect.R;
+import com.example.android.clubsconnect.model.Message;
 
 /**
  * Created by ProfessorTaha on 3/3/2018.
