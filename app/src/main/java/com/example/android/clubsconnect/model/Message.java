b608f1fe56fc2fc0eb215eb4e4db783938969f31
ben-var <ben.var@outlook.com>
2018-03-11 10:28:57 -0700

    methods added to Message class
    - added toString()
    - added equals()
    - added javadocs for each method

--- app/src/main/java/com/example/android/clubsconnect/model/Message.java
+++ app/src/main/java/com/example/android/clubsconnect/model/Message.java
@@ -66,4 +66,51 @@ public class Message implements Serializable {
     public static enum MessageType {
         FROM_ME, FROM_GROUP;
     }
+
+    /**
+     * <b>equals()</b>
+     * <p>
+     *     This method overrides the default equals method. It is used to verify if a message
+     *     is the same message as the passed object.
+     * </p>
+     * @param obj
+     *          - Object passed to the method will be compared for equivalency to Message calling
+     *          this method.
+     * @return
+     *          - true if equals, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof  Message)) {
+            return false;
+        }
+
+        Message other = (Message) obj;
+
+        return other.getTimeSent().equals(this.mTimeSent) &&
+                other.getText().equals(this.mText) &&
+                other.getAuthor().equals(this.mAuthor) &&
+                other.getClub().equals(this.mClub) &&
+                other.getId() == this.mId &&
+                other.getType().equals(this.mType);
+    }
+
+    /**
+     * <b>toString()</b>
+     * <p>
+     *     - This toString should not be used to print the message. It returns a formatted
+     *      string calling the toString of each variable in the Message object.
+     * </p>
+     * @return
+     *     - String dump of all member variables.
+     */
+    @Override
+    public String toString() {
+        return "Text content: " + mText     +
+                "\nID: "        + mId       +
+                "\nClub: "      + mClub     +
+                "\nMsg Type: "  + mType     +
+                "\nAuthor: "    + mAuthor   +
+                "\nTimestamp"   + mTimeSent;
+    }
 }
