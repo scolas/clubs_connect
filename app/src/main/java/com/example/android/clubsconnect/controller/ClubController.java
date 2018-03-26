9701e9ae0c050029c229f517502c758082b50e72
scolas <scolas481@gmail.com>
2018-03-25 22:56:00 -0400

    network call

--- app/src/main/java/com/example/android/clubsconnect/controller/ClubController.java
+++ app/src/main/java/com/example/android/clubsconnect/controller/ClubController.java
@@ -1,5 +1,13 @@
 package com.example.android.clubsconnect.controller;
 
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+
+import com.example.android.clubsconnect.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Created by scott on 3/23/18.
  */
@@ -9,7 +17,6 @@ public class ClubController {
     private String About;
     private String Details;
     private String Name;
-
 
     public String getAbout() {
         return "This is about from controller";
@@ -33,5 +40,42 @@ public class ClubController {
 
     public void setName(String name) {
         Name = name;
+    }
+
+    public ClubController(){
+        makeClubProfileQuery("map");
+    }
+
+    private void makeClubProfileQuery(String clubId){
+        String clubQuery = clubId.toString();
+        URL clubSearchUrl = NetworkUtils.buildUrl(clubQuery);
+        new ClubQueryTask().execute(clubSearchUrl);
+
+    }
+
+    public class ClubQueryTask extends AsyncTask<URL, Void, String>{
+
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String clubSearchResults = null;
+
+            try{
+                clubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return clubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String clubSearchResults) {
+            if(clubSearchResults != null && !clubSearchResults.equals("")){
+                setDetails(clubSearchResults);
+            }else{
+                setDetails("not working");
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/clubsconnect/view/ClubActivity.java
+++ app/src/main/java/com/example/android/clubsconnect/view/ClubActivity.java
@@ -1,18 +1,28 @@
 package com.example.android.clubsconnect.view;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.clubsconnect.R;
 import com.example.android.clubsconnect.controller.ClubController;
+import com.example.android.clubsconnect.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ClubActivity extends AppCompatActivity {
     TextView mNameTextView;
     TextView mDetailTextView;
     TextView mAboutTextView;
     ImageView mImage;
+
+    private ProgressBar mLoadingIndicator;
+
+
     ClubController clubController = new ClubController();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +32,47 @@ public class ClubActivity extends AppCompatActivity {
         mNameTextView = (TextView) findViewById(R.id.club_name_txt);
         mDetailTextView = (TextView) findViewById(R.id.club_detail_txt);
         mAboutTextView = (TextView) findViewById(R.id.club_about_txt);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
         String about = clubController.getAbout();
         mAboutTextView.setText(about);
+       // mDetailTextView.setText(clubController.getDetails());
+    }
+
+
+
+
+
+    private void makeClubProfileQuery(String clubId){
+        String clubQuery = clubId.toString();
+        URL clubSearchUrl = NetworkUtils.buildUrl(clubQuery);
+        new ClubQueryTask().execute(clubSearchUrl);
+
+    }
+
+    public class ClubQueryTask extends AsyncTask<URL, Void, String> {
+
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String clubSearchResults = null;
+
+            try{
+                clubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return clubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String clubSearchResults) {
+            if(clubSearchResults != null && !clubSearchResults.equals("")){
+               // setDetails(clubSearchResults);
+            }else{
+                //setDetails("not working");
+            }
+        }
     }
 }
